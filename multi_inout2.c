836c978e28e388c2fc9420e6af66b4af30afb646
gitakramsm <107043305+gitakramsm@users.noreply.github.com>
2022-09-04 17:29:40 +0530

    Add files via upload

---
+++ multi_inout2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    float num1;
+    double num2;
+
+    printf("enter two decimal numbers: ");
+    scanf("%f%lf",&num1,&num2);
+    printf("you entered %f and %lf",num1,num2);
+}
